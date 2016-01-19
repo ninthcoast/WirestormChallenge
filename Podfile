480fcb4fd46c33c83f64d6ae922a8148bbfe8dfd
Derek Stock <derek@ninthcoast.com>
2016-01-19 15:40:09 -0300

    Added cocoapods with Alamofire and SDWebImage

---
+++ Podfile
@@ -0,0 +1,7 @@
+# platform :ios, ‘8.0’
+use_frameworks!
+
+target ‘WirestormChallenge’ do
+    pod 'SDWebImage', '~>3.7'
+    pod 'Alamofire', '~> 3.0'
+end
---
+++ Pods/Target Support Files/Pods-WirestormChallenge/Pods-WirestormChallenge-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_WirestormChallengeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WirestormChallengeVersionString[];
+
